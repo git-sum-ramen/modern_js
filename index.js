85e56d014f28ab36e92282a3ebb06634a1136c3e
brian <riceb53@gmail.com>
2020-12-09 19:43:58 -0500

    add webpack

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var moment = require('moment'); // require
+
+
 console.log('i am in the js file');
 
 console.log(moment().format('dddd'));
+
+
+
+
+// client side (browser)
+// chrome
+// no access to files/folders
+
+
+// server side js (node)
+// access files/folders
+// can't run in the browser
+
+// webpack convert nodejs to client side js
